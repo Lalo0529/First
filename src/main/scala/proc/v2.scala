0347dd0cb2b0df4c3eacd38783e898c5997b9569
Eduardo Cantero <eduardocantero00@gmail.com>
2018-11-13 15:20:36 -0500

    when the circles interact with each other the background and the fill of the circles changes color

--- src/main/scala/proc/v2.scala
+++ src/main/scala/proc/v2.scala
@@ -4,9 +4,9 @@ import processing.core.PApplet
 import processing.core.PApplet._
 import processing.core.PConstants._
 
-class oop2 extends PApplet {
+class v2 extends PApplet {
   val ScreenDimension = 1080
-var P2: Particle = null
+  var P2: Particle = null
   var P1: Particle = null
   override def settings(): Unit = {
     size(ScreenDimension, ScreenDimension)
@@ -14,7 +14,7 @@ var P2: Particle = null
   }
 
   override def setup(): Unit = {
-P1 = new Particle(100,100,50)
+    P1 = new Particle(100,100,50)
     P2 = new Particle(500,200,100)
   }
 
@@ -26,9 +26,8 @@ P1 = new Particle(100,100,50)
       background(0,255,0)
     }
 
-    if (P1.overlaps(P2)){
-    background(0,255,0)
-    }
+    P1.overlaps(P2)
+
 
     P2.x = mouseX
     P2.y =mouseY
@@ -37,29 +36,33 @@ P1 = new Particle(100,100,50)
     P2.display()
   }
 
-  class Particle(var x: Float, var y: Float, var r: Float) {
+  class Particle(var x: Float, var y: Float, var r: Float , var col: Int = color(0)) {
 
-   def this() = this(
-       random(width),
-       random(height),
-       random(4,18)
+    def this() = this(
+      random(width),
+      random(height),
+      random(4,18)
     )
 
 
 
-    def overlaps(other:Particle)={
+    def overlaps(other:Particle){
       var d: Float = dist(x,y,other.x,other.y)
       if(d < r + other.r ){
-        true
+        col = color (0,0,255,100)
+        other.col = color (255,0,0,100)
       }else{
-        false
+        col = color(0,100)
+        other.col = color(0,250)
+
+
       }
 
     }
 
     def display(): Unit = {
       stroke(255)
-      noFill()
+      fill(col)
       ellipse(x, y, r * 2, r * 2)
     }
 
@@ -68,4 +71,4 @@ P1 = new Particle(100,100,50)
 
 }
 
-object oop2 extends PApplication
+object v2 extends PApplication
